a5025ff301b851b873ce4e5512307fcc10ef147a
FLAVIO AMORUSO <flavioamoruso6.com>
2024-03-01 23:50:51 +0100

    page error animation

--- src/screen/ErrorScreen.js
+++ src/screen/ErrorScreen.js
@@ -1,9 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Lottie from "react-lottie";
+import animationdata from "../assets/animationError.json";
+
+const Wrapper = styled.section`
+  min-height: 86.2vh;
+  display: grid;
+  display: --ms-grid;
+  display: --moz-grid;
+  display: --web-kit-grid;
+  place-items: center;
+  h1 {
+    text-transform: uppercase;
+    text-align: center;
+  }
+  @media screen and (min-width: 992px) {
+    min-height: 84.5vh;
+  }
+`;
 
 const ErrorScreen = () => {
   return (
     <>
-      <h1>Errore 404</h1>
+      <Wrapper>
+        <h1>Pagina non trovata</h1>
+        <Lottie
+          options={{
+            loop: true,
+            autoplay: true,
+            reverse: true,
+            animationData: animationdata,
+            rendererSettings: {
+              preserveAspectRatio: "xMidYMid slice",
+            },
+          }}
+          width={300}
+          height={300}
+        />
+        <Link to="/" className="btn btn-primary">
+          Torna indietro
+        </Link>
+      </Wrapper>
     </>
   );
 };
