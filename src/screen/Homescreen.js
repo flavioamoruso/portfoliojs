aa1412e0f41a1565f13836f9f355f917b95f01db
FLAVIO AMORUSO <flavioamoruso6.com>
2024-03-04 14:36:07 +0100

    lottie e button

--- src/screen/Homescreen.js
+++ src/screen/Homescreen.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Lottie from "react-lottie";
-import animationHome from "../assets/animation/animationHome.json";
+import animationData from "../assets/animation/animationHome.json";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const HomeScreen = () => {
   return (
@@ -9,24 +10,34 @@ const HomeScreen = () => {
       <div className="home-hero">
         <div className="home-hero-text">
           <div className="home-hero-title">
-            <h2 className="brand-color"> Salve a tutti</h2>
-            <h4>Mi chiamo Flavio Amoruso e sono un web developer junior</h4>
+            <h2 className="brand-color focus-in-contract-bck">
+              {" "}
+              Salve a tutti
+            </h2>
+            <h4 className="focus-in-contract-bck">
+              Mi chiamo Flavio Amoruso e sono un web developer junior
+            </h4>
           </div>
-          <p>Questo è il mio sito web personale dove mostro le mie skills</p>
-          <Link to="/projects" className="btn btn-primary">
+
+          <p className="focus-in-contract-bck">
+            Questo è il mio sito web personale dove mostro le mie skills
+          </p>
+
+          <Link
+            to="/projects"
+            className="btn btn-primary focus-in-contract-bck"
+          >
             Scopri di più
           </Link>
         </div>
-        <div className="home-hero-img">
+        <div className="home-hero-img focus-in-contract-bck">
           <Lottie
+            className=""
             options={{
               loop: true,
               autoplay: true,
               reverse: true,
-              animationHome: animationHome,
-              rendererSettings: {
-                preserveAspectRatio: "xMidYMid slice",
-              },
+              animationData,
             }}
             width={300}
             height={300}
