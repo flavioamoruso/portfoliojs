4b8193e41255ddf94d15673739d9e9fb48b838f9
FLAVIO AMORUSO <flavioamoruso6.com>
2024-03-08 20:24:27 +0100

    card ripristinate

--- src/components/products.js
+++ src/components/products.js
@@ -3,20 +3,7 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
 export function Products(props) {
-  return (
-    <Row xs={6} md={12} className="g-1">
-      {Array.from({ length: 3 }).map((_, idx) => (
-        <Col key={idx}>
-          <Card>
-            <Card.Body>
-              <Card.Title>{props.title}</Card.Title>
-              <Card.Text>{props.body}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
-  );
+  return <></>;
 }
 
 export default Products;
