d2eea471ba143d04d142a525082959fce74259e5
FLAVIO AMORUSO <flavioamoruso6.com>
2024-03-08 21:23:44 +0100

    input rifatto

--- src/screen/Contact.js
+++ src/screen/Contact.js
@@ -9,63 +9,142 @@ function Animation() {
 }
 
 const StyledForm = styled.form`
-  .form-container {
+  .form {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    max-width: 350px;
+    padding: 20px;
+    border-radius: 20px;
+    position: relative;
+    background-color: #1a1a1a;
+    color: #fff;
+    border: 1px solid #333;
+  }
+
+  .title {
+    font-size: 28px;
+    font-weight: 600;
+    letter-spacing: -1px;
+    position: relative;
+    display: flex;
+    align-items: center;
+    padding-left: 30px;
+    color: #00bfff;
+  }
+
+  .title::before {
+    width: 18px;
+    height: 18px;
+  }
+
+  .title::after {
+    width: 18px;
+    height: 18px;
+    animation: pulse 1s linear infinite;
+  }
+
+  .title::before,
+  .title::after {
+    position: absolute;
+    content: "";
+    height: 16px;
+    width: 16px;
+    border-radius: 50%;
+    left: 0px;
+    background-color: #00bfff;
+  }
+
+  .message,
+  .signin {
+    font-size: 14.5px;
+    color: rgba(255, 255, 255, 0.7);
+  }
+
+  .signin {
     text-align: center;
-    margin: 30px;
-    background-color: var(--input-back);
-    padding: 35px;
-    border-left: 5px solid #002733;
-    clip-path: polygon(
-      0 0,
-      100% 0,
-      100% calc(100% - 20px),
-      calc(100% - 20px) 100%,
-      0 100%
-    );
   }
 
-  .heading {
-    display: block;
-    color: var(--text-input);
-    font-size: 1.5rem;
-    font-weight: 800;
-    margin-bottom: 20px;
+  .signin a:hover {
+    text-decoration: underline royalblue;
   }
 
-  .form-container .form .input {
-    color: white;
+  .signin a {
+    color: #00bfff;
+  }
+
+  .flex {
+    display: flex;
     width: 100%;
-    background-color: #002733;
+    gap: 6px;
+  }
+
+  .form label {
+    position: relative;
+  }
+
+  .form label .input {
+    background-color: #333;
+    color: #fff;
+    width: 100%;
+    padding: 20px 05px 05px 10px;
+    outline: 0;
+    border: 1px solid rgba(105, 105, 105, 0.397);
+    border-radius: 10px;
+  }
+
+  .form label .input + span {
+    color: rgba(255, 255, 255, 0.5);
+    position: absolute;
+    left: 10px;
+    top: 0px;
+    font-size: 0.9em;
+    cursor: text;
+    transition: 0.3s ease;
+  }
+
+  .form label .input:placeholder-shown + span {
+    top: 12.5px;
+    font-size: 0.9em;
+  }
+
+  .form label .input:focus + span,
+  .form label .input:valid + span {
+    color: #00bfff;
+    top: 0px;
+    font-size: 0.7em;
+    font-weight: 600;
+  }
+
+  .input {
+    font-size: medium;
+  }
+
+  .submit {
     border: none;
     outline: none;
     padding: 10px;
-    margin-bottom: 20px;
-    font-weight: bold;
-    transition: all 0.2s ease-in-out;
-    border-left: 1px solid transparent;
+    border-radius: 10px;
+    color: #fff;
+    font-size: 16px;
+    transform: 0.3s ease;
+    background-color: #00bfff;
   }
 
-  .form-container .form .input:focus {
-    border-left: 5px solid var(--nav-colors-2);
+  .submit:hover {
+    background-color: #00bfff96;
   }
 
-  .form-container .form .textarea {
-    width: 100%;
-    padding: 10px;
-    border: none;
-    outline: none;
-    background-color: #013747;
-    color: white;
-    font-weight: bold;
-    resize: none;
-    max-height: 150px;
-    margin-bottom: 20px;
-    border-left: 1px solid transparent;
-    transition: all 0.2s ease-in-out;
-  }
+  @keyframes pulse {
+    from {
+      transform: scale(0.9);
+      opacity: 1;
+    }
 
-  .form-container .form .textarea:focus {
-    border-left: 5px solid var(--nav-colors-2);
+    to {
+      transform: scale(1.8);
+      opacity: 0;
+    }
   }
 `;
 
@@ -73,37 +152,32 @@ const Contact = () => {
   return (
     <StyledForm data-aos="zoom-in">
       <form action="https://formspree.io/f/mnqezqlw" method="POST">
-        <div class="form-container">
-          <div class="form">
-            <span class="heading">Contattami</span>
-            <input placeholder="Nome" type="text" class="input" id="nome" />
-            <input
-              placeholder="Cognome"
-              type="text"
-              class="input"
-              id="cognome"
-            />
-            <input
-              placeholder="Telefono (+39 333 70 30 821)"
-              type="number"
-              class="input"
-              id="telefono"
-            />
-            <input placeholder="Email" id="mail" type="email" class="input" />
-            <textarea
-              placeholder="Cosa ti serve?"
-              rows="10"
-              cols="30"
-              id="message"
-              name="message"
-              class="textarea"
-            ></textarea>
+        <form class="form">
+          <p class="title">Contattami </p>
+          <div class="flex">
+            <label>
+              <input className="input" type="text" placeholder="" required="" />
+              <span>Nome</span>
+            </label>
 
-            <button class="btn" style={{ padding: "16px", cursor: "pointer" }}>
-              Invia
-            </button>
+            <label>
+              <input className="input" type="text" placeholder="" required="" />
+              <span>Cognome</span>
+            </label>
           </div>
-        </div>
+
+          <label>
+            <input className="input" type="email" placeholder="" required="" />
+            <span>Email</span>
+          </label>
+          <label>
+            <textarea placeholder="" className="input"></textarea>
+            <span>Richiesta</span>
+          </label>
+          <button style={{ cursor: "pointer" }} type="submit" class="submit">
+            Invia
+          </button>
+        </form>
       </form>
     </StyledForm>
   );
