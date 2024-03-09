08ae369cd145ad525a8b17bfaca7607d50a4b090
FLAVIO AMORUSO <flavioamoruso6.com>
2024-03-09 20:53:24 +0100

    form aggiustato

--- src/screen/Contact.js
+++ src/screen/Contact.js
@@ -8,178 +8,71 @@ function Animation() {
   }, []);
 }
 
-const StyledForm = styled.form`
-  .form {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    max-width: 350px;
-    padding: 20px;
-    border-radius: 20px;
-    position: relative;
-    background-color: #1a1a1a;
-    color: #fff;
-    border: 1px solid #333;
-  }
-
-  .title {
-    font-size: 28px;
-    font-weight: 600;
-    letter-spacing: -1px;
-    position: relative;
-    display: flex;
-    align-items: center;
-    padding-left: 30px;
-    color: #00bfff;
-  }
-
-  .title::before {
-    width: 18px;
-    height: 18px;
-  }
-
-  .title::after {
-    width: 18px;
-    height: 18px;
-    animation: pulse 1s linear infinite;
-  }
-
-  .title::before,
-  .title::after {
-    position: absolute;
-    content: "";
-    height: 16px;
-    width: 16px;
-    border-radius: 50%;
-    left: 0px;
-    background-color: #00bfff;
-  }
-
-  .message,
-  .signin {
-    font-size: 14.5px;
-    color: rgba(255, 255, 255, 0.7);
-  }
-
-  .signin {
-    text-align: center;
-  }
-
-  .signin a:hover {
-    text-decoration: underline royalblue;
-  }
-
-  .signin a {
-    color: #00bfff;
-  }
-
-  .flex {
-    display: flex;
-    width: 100%;
-    gap: 6px;
-  }
-
-  .form label {
-    position: relative;
-  }
-
-  .form label .input {
-    background-color: #333;
-    color: #fff;
-    width: 100%;
-    padding: 20px 05px 05px 10px;
-    outline: 0;
-    border: 1px solid rgba(105, 105, 105, 0.397);
-    border-radius: 10px;
-  }
-
-  .form label .input + span {
-    color: rgba(255, 255, 255, 0.5);
-    position: absolute;
-    left: 10px;
-    top: 0px;
-    font-size: 0.9em;
-    cursor: text;
-    transition: 0.3s ease;
-  }
-
-  .form label .input:placeholder-shown + span {
-    top: 12.5px;
-    font-size: 0.9em;
-  }
-
-  .form label .input:focus + span,
-  .form label .input:valid + span {
-    color: #00bfff;
-    top: 0px;
-    font-size: 0.7em;
-    font-weight: 600;
-  }
-
-  .input {
-    font-size: medium;
-  }
-
-  .submit {
-    border: none;
-    outline: none;
-    padding: 10px;
-    border-radius: 10px;
-    color: #fff;
-    font-size: 16px;
-    transform: 0.3s ease;
-    background-color: #00bfff;
-  }
-
-  .submit:hover {
-    background-color: #00bfff96;
-  }
-
-  @keyframes pulse {
-    from {
-      transform: scale(0.9);
-      opacity: 1;
-    }
-
-    to {
-      transform: scale(1.8);
-      opacity: 0;
-    }
-  }
-`;
-
 const Contact = () => {
   return (
-    <StyledForm data-aos="zoom-in">
-      <form action="https://formspree.io/f/mnqezqlw" method="POST">
-        <form class="form">
-          <p class="title">Contattami </p>
-          <div class="flex">
-            <label>
-              <input className="input" type="text" placeholder="" required="" />
-              <span>Nome</span>
-            </label>
-
-            <label>
-              <input className="input" type="text" placeholder="" required="" />
-              <span>Cognome</span>
-            </label>
+    <>
+      <div className="home-hero" data-aos="fade-left">
+        <div className="home-hero-text">
+          <div className="home-hero-title">
+            <h3 className="brand-color">
+              Per maggiori informazioni non esitate a contattarmi
+            </h3>
+            <p>Email: flavioamoruso6@gmail.com</p>
+            <p>Telefono: +39 3703081095</p>
           </div>
+        </div>
+        <div className="home-hero-img focus-in-contract-bck">
+          <form
+            className="form"
+            action="https://formspree.io/f/xwkgqqpa"
+            method="POST"
+          >
+            <p class="title">Contattami </p>
+            <div class="flex">
+              <label>
+                <input
+                  className="input"
+                  type="text"
+                  name="name"
+                  placeholder=""
+                  required=""
+                />
+                <span>Nome</span>
+              </label>
 
-          <label>
-            <input className="input" type="email" placeholder="" required="" />
-            <span>Email</span>
-          </label>
-          <label>
-            <textarea placeholder="" className="input"></textarea>
-            <span>Richiesta</span>
-          </label>
-          <button style={{ cursor: "pointer" }} type="submit" class="submit">
-            Invia
-          </button>
-        </form>
-      </form>
-    </StyledForm>
+              <label>
+                <input
+                  className="input"
+                  type="text"
+                  name="surname"
+                  placeholder=""
+                  required=""
+                />
+                <span>Cognome</span>
+              </label>
+            </div>
+
+            <label>
+              <input
+                className="input"
+                type="email"
+                name="email"
+                placeholder=""
+                required=""
+              />
+              <span>Email</span>
+            </label>
+            <label>
+              <textarea placeholder="" name="message" className="input" />
+              <span>Richiesta</span>
+            </label>
+            <button style={{ cursor: "pointer" }} type="submit" class="submit">
+              Invia
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
