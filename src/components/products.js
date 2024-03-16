de189f829483392ad9fb30f9cbf92b3b0c5f4b7d
FLAVIO AMORUSO <flavioamoruso6.com>
2024-03-16 19:18:24 +0100

    dettagli

--- src/components/products.js
+++ src/components/products.js
@@ -7,30 +7,6 @@ export function Products(props) {
         style={{ marginTop: "40px", marginBottom: "20px" }}
         data-aos="fade-up"
       >
-        {/* <div class="temporary__storage">
-          <div class="card">
-            <img src={props.img} alt="product-img" className="image"></img>
-            <div class="image__overlay"></div>
-            <div class="content">
-              <div class="avatar"></div>
-              <div class="content__text">
-                <span class="stream__title">
-                  {props.icon}
-                  {props.title}
-                </span>
-                <div class="content__body">
-                  <span class="event">{props.body}</span>
-                </div>
-                <button className="btn categories__btn">
-                  <a href={props.code}>
-                    GitHub <FaGithub />
-                  </a>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div> */}
-
         <article class="card">
           <div class="card-img">
             <div class="card-imgs pv delete"></div>
